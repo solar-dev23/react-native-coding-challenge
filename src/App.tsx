cbd08e8d0fb951c701e376f2995d834d6bfe409c
Jayden Jin <jaydenjin223@outlook.com>
2023-03-01 13:10:20 -0700

    Change app.tsx location

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {Provider} from 'react-redux';
-import RootNavigator from './src/navigators/RootNavigator';
-import {store} from './src/store';
+import RootNavigator from './navigators/RootNavigator';
+import {store} from './store';
 
 function App(): JSX.Element {
   return (
